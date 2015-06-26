a1dca416276c626e5e2e82bc5ac471c4a7fc4079
Phil Weir <phil.weir@numa.ie>
2015-06-26 14:07:52 +0100

    match up with master (minor typos)

--- numa-elmer/NumaCellState/NumaCellState.f90
+++ numa-elmer/NumaCellState/NumaCellState.f90
@@ -17,7 +17,8 @@
 !  * You should have received a copy of the GNU Affero General Public License
 !  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 !  */
-! 
+!
+
 SUBROUTINE NumaCellStateSolver_init( Model, Solver, Timestep, TransientSimulation)
        USE DefUtils
 
